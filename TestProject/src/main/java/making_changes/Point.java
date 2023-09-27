98aa4ef8df9caed27556f7279e72e8ae2c1bbb7a
Paul Woods <paul.n.woods@gmail.com>
2023-09-26 17:51:03 -0700

    Fixed errors / double method generation in prior version.

--- TestProject/src/main/java/making_changes/Point.java
+++ TestProject/src/main/java/making_changes/Point.java
@@ -7,10 +7,13 @@ public class Point {
     private double y;
     private double z;
 
-    public Point(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
     }
 
     @Override
@@ -52,47 +55,6 @@ public class Point {
 
     public void setZ(double z) {
         this.z = z;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Point point = (Point) o;
-        return Double.compare(x, point.x) == 0 && Double.compare(y, point.y) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
-    @Override
-    public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
     }
 }
 
