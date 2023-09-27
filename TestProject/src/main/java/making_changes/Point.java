0b5f5077e5c85b74310b0c4f2f9d3c4bcf335a81
Paul Woods <paul.n.woods@gmail.com>
2023-09-26 17:34:28 -0700

    Converted Point into a three dimensional property.

--- TestProject/src/main/java/making_changes/Point.java
+++ TestProject/src/main/java/making_changes/Point.java
@@ -1,11 +1,56 @@
 package making_changes;
 
+import java.util.Objects;
+
 public class Point {
     private double x;
     private double y;
+    private double z;
 
-    public Point(double x, double y) {
+    public Point(double x, double y, double z) {
         this.x = x;
         this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return Double.compare(x, point.x) == 0 && Double.compare(y, point.y) == 0 && Double.compare(z, point.z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
     }
 }
